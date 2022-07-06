275364bbe54b4179ce18abc1df56fc7eac758a2d
EC2 Default User <106435322+show->
2022-07-06 06:11:11 +0000

    5finish

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
     before_action :authenticate_user!
     def search
         @range = params[:range]
+        @word = params[:word]
         if @range == "User"
             @users = User.looks(params[:search],params[:word])
         else
